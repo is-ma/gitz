540c9431a6341c9571c6110756e88c250bb2f291
Is Ma <dotpointing@gmail.com>
2018-10-10 07:09:57 -0500

    add gxu, gxj, gxz; rename gr -> grb; improve comments

--- git_shortcuts.sh
+++ git_shortcuts.sh
@@ -12,7 +12,7 @@ alias ga='git add'
 alias gaa='git add -A'
 alias gc='git commit -m'
 alias gca='git commit -am'
-alias gcm='git commit --amend' # Add more files to last commit or edit the commited message
+alias gcm='git commit --amend' # add more files to last commit or edit the commited message
 alias gt='git tag'             # usage: gt -a v1.0; gt -d v1.0 (delete tag); tag after commit: gt v1.2 fceb02
                                # remove online tag: git push --delete origin [tagName] (view branch removing);
                                # to delete all tags: tag | xargs git tag -d
@@ -24,31 +24,31 @@ alias gl='git log --oneline --graph --decorate --all -n 10'           # top 10, 
 alias gl3='git log --oneline -n 3'                                    # top 3, simplified
 alias gls='git log --oneline --graph --decorate --all -n 3 --stat'    # top 3, with stats
 alias gll='git log --oneline --graph --decorate --all'                # all git log, complete info
-alias gd='git diff'            # Modifs on tracked files (WD) above HEAD (could be staged). Usages: gd; gd <OLD> <NEW>
+alias gd='git diff'            # modifs on tracked files (WD) above HEAD (could be staged). Usages: gd; gd <OLD> <NEW>
 alias gds='git diff --staged'  # INDEXED modifs above HEAD that (could be commited)
-alias gdh='git diff HEAD'      # Modifs [WD+INDEX] above HEAD
-alias gsh='git show'           # Show commit modifications: gsh [COMMIT|file]
+alias gdh='git diff HEAD'      # modifs [WD+INDEX] above HEAD
+alias gsh='git show'           # show commit modifications: gsh [COMMIT|file]
 alias gfh='git log -p'         # (+filename) Outputs the file history patches for each log entry
 
 
 ########## branches ##########
-alias gco='git checkout'                # Jump to branches or IDs. Add '-b' create a new branch (opc FromBranch)
+alias gco='git checkout'                # jump to branches or IDs. Add '-b' create a new branch (opc FromBranch)
                                         # with -f, throw away local changes
 alias gb='git branch'                   # (opc BranchName) Show local branches, delete it (-d, -D), remote (-r),
                                         # all (-a), rename branches with "gb -m develop developing"
 alias gm='git merge'                    # (+COMMIT) Merge current branch to COMMIT (fast-forward) or use --no-ff flag
-alias gr='git rebase'                   # (+COMMIT) Rebase current branch on top of COMMIT
+alias grb='git rebase'                  # (+COMMIT) Rebase current branch on top of COMMIT
 alias gri='git rebase -i'               # (+COMMIT) Re-design history interactively from COMMIT. Add --root to use it.
-alias gp='git push'                     # Use -f to mimic current branch; delete branches: gp --delete origin branchName
-alias gpt='git push && git push --tags' # Pushes commits and tags in one command
-alias gf='git fetch -p'                 # Usage: gf john (-p prune tracking); -a to fetch all; uses "origin" by default
-# git pull                              # Fetches & merges (use it with care), --rebase opt is a great one-command tool
+alias gp='git push'                     # use -f to mimic current branch; delete branches: gp --delete origin branchName
+alias gpt='git push && git push --tags' # pushes commits and tags in one command
+alias gf='git fetch -p'                 # usage: gf john (-p prune tracking); -a to fetch all; uses "origin" by default
+alias gpu='git pull'                    # fetches & merges (use it with care), --rebase opt is a great one-command tool
 
 
 ########## stash commands ##########
-alias gss='git stash save'                    # Save uncommited changes (add an ID message)
-alias gsl='git stash list'                    # Your list of stashed stuff
-alias gsp='git stash pop'                     # Takes (and delete) the top of the list stash
+alias gss='git stash save'              # save uncommited changes (add an ID message)
+alias gsl='git stash list'              # your list of stashed stuff
+alias gsp='git stash pop'               # takes (and delete) the top of the list stash
 
 
 ########## config ##########
@@ -59,28 +59,20 @@ alias gsp='git stash pop'                     # Takes (and delete) the top of th
 
 
 ########## undoing things ##########
-alias gcn='git clean -n'             # Removes custom/all untracked files (-n for dry-run)
-alias gcf='git clean -f'             # Removes custom/all untracked files (-f to force)
-# git reset ID                       # Moves current branch to ID and
-                                           # --soft keep changes in INDEX
-                                           # --mixed (default) keep changes in working dir
-                                           # --hard discard changes in working dir and INDEX
-# git reset [HEAD] --soft            # it does nothing
-# git reset [HEAD] [--mixed]         # it unstage all files
-# git reset [HEAD] --hard            # it just removes modified files in working dir and INDEX
-# git reset [HEAD] stagedFile        # it unstage a staged file
-# git reset HEAD^ --soft             # go back 1 commit in time, keeps modifications in INDEX
-# git reset HEAD^ [--mixed]          # go back 1 commit in time, keeps modifications in WD
-# git reset HEAD^ --hard             # go back 1 commit in time but destroy your WD and INDEX files
-# git checkout [--] (.|file)         # Un-do file changes in WD (use -- when it doesn't work, paths for example),
+alias gcn='git clean -n'             # removes custom/all untracked files (-n for dry-run)
+alias gcf='git clean -f'             # removes custom/all untracked files (-f to force)
+alias gxu='git reset'                # (+stagedFile, empty) unstage all files OR stagedFile: [HEAD] --mixed
+alias gxj='git reset --hard'         # (+ID, empty) jumps to ID or removes modified files in working dir and INDEX
+alias gxz='git reset HEAD^ --soft'   # go back 1 commit in time, keeps modifications in INDEX (like a Ctrl+Z)
+# git checkout [--] (.|file)         # un-do file changes in WD (use -- when it doesn't work, paths for example),
                                      # or use '.' to undo all WD (same as git checkout -f or git reset --hard)
 
 ########## final references ##########
-# git init            # Initialize repository. Add --bare for "storage facility" without working dir
-# git remote          # Use it alone or with [add, rm] +repo, -v for details
-# git rm              # Untrack files; "git rm --cached" can unstage untracked-files
-# git mv              # Rename tracked-files
-# git reflog          # Revive lost snapshots
-# git archive         # Removes the .git dir; it gives you a simple snapshot you to share/backup,
-                      # Ex: git archive master --format=zip --output=../website-20170130.zip
+# git init            # initialize repository. Add --bare for "storage facility" without working dir
+# git remote          # use it alone or with [add, rm] +repo, -v for details
+# git rm              # untrack files; "git rm --cached" can unstage untracked-files
+# git mv              # rename tracked-files
+# git reflog          # revive lost snapshots
+# git archive         # removes the .git dir; it gives you a simple snapshot you to share/backup,
+                      # ex: git archive master --format=zip --output=../website-20170130.zip
 
