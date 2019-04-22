e68597ca8cfd7c9e285fa3607e25cebe1a23b676
Is Ma <dotpointing@gmail.com>
2019-04-22 04:53:47 -0500

    Improve documentation.

--- git_shortcuts.sh
+++ git_shortcuts.sh
@@ -10,69 +10,69 @@ export VISUAL=vim  # use VIM instead of nano as a default editor
 alias gs='git status -s'
 alias ga='git add'
 alias gaa='git add -A'
-alias gc='git commit -m'
+alias gc='git commit -m'                  # You could add (--allow-empty).
 alias gca='git commit -am'
-alias gaac='git add -A && git commit -m'  # common combination
-alias gcm='git commit --amend'            # add more files to last commit or edit the commited message
-alias gt='git tag'                        # usage: gt -a v1.0; gt -d v1.0 (delete tag); tag after commit: gt v1.2 fceb02
-alias god='git push --delete origin'      # (+tagName) ask origin to delete a tag/branch
+alias gaac='git add -A && git commit -m'  # Common combination.
+alias gcm='git commit --amend'            # Add more files to last commit or edit the commited message.
+alias gt='git tag'                        # Usage: gt v1.0; gt -d v1.0 (delete tag); tag after commit: gt v1.2 fceb02.
+alias god='git push --delete origin'      # (+tagName) Goes to origin and delete tags/branches.
 
 
 ########## information ##########
-alias gs='git status -s'
-alias gl='git log --oneline --graph --decorate --all -n 10'           # top 10, complete info
-alias gl3='git log --oneline -n 3'                                    # top 3, simplified
-alias gls='git log --oneline --graph --decorate --all -n 3 --stat'    # top 3, with stats
-alias gll='git log --oneline --graph --decorate --all'                # all git log, complete info
-alias gd='git diff'            # modifs on tracked files (WD) above HEAD (could be staged). Usages: gd; gd <OLD> <NEW>
-alias gds='git diff --staged'  # INDEXED modifs above HEAD that (could be commited)
-alias gdh='git diff HEAD'      # modifs [WD+INDEX] above HEAD
-alias gsh='git show'           # show commit modifications: gsh [COMMIT|file]
-alias gfh='git log -p'         # (+filename) Outputs the file history patches for each log entry
+alias gl='git log --oneline --graph --decorate --all -n 10'           # Top 10, complete info.
+alias gl3='git log --oneline -n 3'                                    # Top 3, simplified.
+alias gls='git log --oneline --graph --decorate --all -n 3 --stat'    # Top 3, with stats.
+alias gll='git log --oneline --graph --decorate --all'                # All git log, complete info.
+alias gd='git diff'            # Modifs on tracked files (WD) above HEAD (could be staged). Usages: gd; gd <OLD> <NEW>.
+alias gds='git diff --staged'  # INDEXED modifs above HEAD that (could be commited).
+alias gdh='git diff HEAD'      # Modifs [WD+INDEX] above HEAD.
+alias gsh='git show'           # Show commit modifications: gsh [COMMIT|file].
+alias gfh='git log -p'         # (+filename) Outputs the file history patches for each log entry.
 
 
 ########## branches ##########
-alias gco='git checkout'                    # jump to branches or IDs. Add '-b' create a new branch (opc FromBranch)
-                                            # (+filename) remove it from WD; with -f, throw away local changes
+alias gco='git checkout'                    # Jump to branches or IDs. Add '-b' create a new branch (opc FromBranch).
+                                            # (+filename) Remove changes from WD; with -f, throw away local changes.
 alias gb='git branch'                       # (opc BranchName) Show local branches, delete it (-d, -D) or god for remote,
-                                            # remote (-r), all (-a), rename branches with "gb -m develop developing"
-alias gm='git merge'                        # (+COMMIT) Merge current branch to COMMIT (fast-forward) or use --no-ff flag
-alias grb='git rebase'                      # (+COMMIT) Rebase current branch on top of COMMIT
+                                            # remote (-r), all (-a), rename branches with "gb -m develop developing".
+alias gm='git merge'                        # (+COMMIT) Merge current branch to COMMIT (fast-forward) or use --no-ff flag.
+alias grb='git rebase'                      # (+COMMIT) Rebase current branch on top of COMMIT.
 alias gri='git rebase -i'                   # (+COMMIT/--root) Re-design history interactively from COMMIT.
-alias gp='git push && git push --tags'      # pushes commits and tags in one command; (-f) to mimic current branch
-alias gpf='git push -f && git push --tags'  # force push to mimic current branch
-alias gf='git fetch -p'                     # usage: gf john (-p prune tracking); -a to fetch all; uses "origin" by default
-alias gpu='git pull'                        # fetches & merges (use it with care), --rebase opt is a great one-command tool
+alias gp='git push && git push --tags'      # Pushes commits and tags in one command; (-f) to mimic current branch.
+alias gpf='git push -f && git push --tags'  # Force push to mimic current branch.
+alias gf='git fetch -p'                     # Usage: gf john (-p prune tracking); -a to fetch all; uses "origin" by default.
+alias gpu='git pull'                        # Fetches & merges (use it with care), --rebase opt is a great one-command tool.
 
 
 ########## stash commands ##########
-alias gss='git stash save'              # save uncommited changes (add an ID message)
-alias gsl='git stash list'              # your list of stashed stuff
-alias gsp='git stash pop'               # takes (and delete) the top of the list stash
+alias gss='git stash save'              # Save uncommited changes (add an ID message).
+alias gsl='git stash list'              # Your list of stashed stuff.
+alias gsp='git stash pop'               # Takes (and delete) the top of the list stash.
 
 
 ########## config ##########
-# git config --global --edit
-# git config --local --edit
+# git config --global --edit            # Stores configuration in ~/.gitconfig
+# git config --local --edit             # (default) Stores configuration in .git/config
 # git config --local user.name "Example"
 # git config --local user.email "user@example.org"
 
 
 ########## undoing things ##########
-alias gcn='git clean -n'             # removes custom/all untracked files (-n for dry-run)
-alias gcf='git clean -f'             # removes custom/all untracked files (-f to force)
-alias gr='git reset'                 # (+stagedFile, empty) unstage stagedFile or all files
-alias gxj='git reset --hard'         # (+ID, empty) jumps to ID or removes modified files in working dir and INDEX
-alias gxb='git reset HEAD^ --soft'   # go back 1 commit in time, keeps modifications in INDEX
-# git checkout [--] (.|file)         # un-do file changes in WD (use -- when it doesn't work, paths for example),
-                                     # or use '.' to undo all WD (same as git checkout -f or git reset --hard)
+alias gcn='git clean -n'             # Removes custom/all untracked files (-n for dry-run).
+alias gcf='git clean -f'             # Removes custom/all untracked files (-f to force).
+alias gr='git reset'                 # (+stagedFile, empty) Unstage stagedFile or all files.
+alias gxj='git reset --hard'         # (+ID, empty) Jumps to ID or removes modified files in working dir and INDEX.
+alias gxb='git reset HEAD^ --soft'   # Go back 1 commit in time, keeps modifications in INDEX.
+alias grv='git revert'               # (+ID) Reverts ID; (ID ID2 ID3) reverts ids; (BASE..ID) FROM BASE up to ID.
+# git checkout [--] (.|file)         # Un-do file changes in WD (use -- when it doesn't work, paths for example),
+                                     # or use '.' to undo all WD (same as git checkout -f or git reset --hard).
 
 ########## final references ##########
-# git init            # initialize repository. Add --bare for "storage facility" without working dir
-# git remote          # use it alone or with [add, rm] +repo, -v for details
-# git rm              # untrack files; "git rm --cached" can unstage untracked-files
-# git mv              # rename tracked-files
-# git reflog          # revive lost snapshots
-# git archive         # removes the .git dir; it gives you a simple snapshot you to share/backup,
+# git init            # Initialize repository. Add --bare for "storage facility" without working dir.
+# git remote          # Use it alone or with [add, rm] +repo, -v for details.
+# git rm              # Untrack files; "git rm --cached" can unstage untracked-files.
+# git mv              # Rename tracked-files.
+# git reflog          # Revive lost snapshots.
+# git archive         # Removes the .git dir; it gives you a simple snapshot you to share/backup,
                       # ex: git archive master --format=zip --output=../website-20170130.zip
 
