76bdda83dfa3abf9530edb14c7bdd318a3094aeb
Is Ma <dotpointing@gmail.com>
2023-05-19 11:37:52 -0600

    Add silent source

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -3,3 +3,6 @@ cat << EOF >> ~/.bashrc
 ### IS-MA: GITZ ###
 source ~/.is-ma/gitz/shortcuts.sh
 EOF
+
+# silent source
+source ~/.bashrc > /dev/null 2>&1
--- install_on_macos.sh
+++ install_on_macos.sh
@@ -1,5 +1,8 @@
-cat << EOF >> ~/.bashrc
+cat << EOF >> ~/.bash_profile
 
 ### IS-MA: GITZ ###
 source ~/.is-ma/gitz/shortcuts.sh
 EOF
+
+# silent source
+source ~/.bash_profile > /dev/null 2>&1
